4183dba7db97c66594ba7061e0ce6a3e530f89a3
lahotikunal <37633212+lahotikunal@users.noreply.github.com>
2020-10-27 23:33:43 +0530

    Update index7java.js

--- index7java.js
+++ index7java.js
@@ -1,6 +1,9 @@
 
 
-rules.onclick = function () {
-      {location.href = "https://lahotikunal.github.io/insurance_f/";}
-  
-};
+rules.addEventListener('click', function() {
+  //document.getElementById('text-area').innerHTML = "Put the bid, and"+"<br>"+"select one of"+"<br>"+"<7 , =7 or >7 and,"+"<br>"+"roll the dice."+"<br>"+"Have Fun!";
+  //document.getElementById('rules').style.display = 'none';
+
+	{location.href = "https://codepen.io/kunal28/pen/pobjEoX";}
+
+});
